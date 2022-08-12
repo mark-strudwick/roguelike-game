75c96dd7c5ad56b20aba0e6d4d1073ab9ee07ad5
Mark <markstrudwick@outlook.com>
2022-08-12 23:35:58 +0100

    Objects and Map

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use tcod::console::*;
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+
 const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
 
 struct Tcod {
@@ -12,7 +18,55 @@ struct Tcod {
     con: Offscreen,
 }
 
+type Map = Vec<Vec<Tile>>;
+
+struct Game {
+    map: Map,
+}
+
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    map
+}
+
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+    // draw all objects in the list
+    for object in objects {
+        object.draw(&mut tcod.con);
+    }
+
+    // go through all tiles, and set their background color
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = game.map[x as usize][y as usize].block_sight;
+            if wall {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    // blit the contents of "con" to the root console and present it
+    blit(
+        &tcod.con,
+        (0, 0),
+        (SCREEN_WIDTH, SCREEN_HEIGHT),
+        &mut tcod.root,
+        (0, 0),
+        1.0,
+        1.0,
+    );
+}
+
 /// This is a generic object: the player, a monster, an item
+#[derive(Debug)]
 struct Object {
     x: i32,
     y: i32,
@@ -26,9 +80,11 @@ impl Object {
     }
 
     /// Move by the given amount
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
     }
 
     /// Set the color and then draw the character that represent this object as it's position
@@ -38,17 +94,41 @@ impl Object {
     }
 }
 
-fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
+/// A tile of the map and it's properties
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+
+fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
     let key = tcod.root.wait_for_keypress(true);
     match key {
         // movement keys
-        Key { code: Up, .. } => player.move_by(0, -1), 
-        Key { code: Down, .. } => player.move_by(0, 1),
-        Key { code: Left, .. } => player.move_by(-1, 0),
-        Key { code: Right, .. } => player.move_by(1, 0),
+        Key { code: Up, .. } => player.move_by(0, -1, game), 
+        Key { code: Down, .. } => player.move_by(0, 1, game),
+        Key { code: Left, .. } => player.move_by(-1, 0, game),
+        Key { code: Right, .. } => player.move_by(1, 0, game),
         Key {
             code: Enter,
             alt: true,
@@ -75,7 +155,7 @@ fn main() {
         .title("Roguelike Game")
         .init();
 
-    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
     let mut tcod = Tcod { root, con };
 
     let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
@@ -83,6 +163,11 @@ fn main() {
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
 
     let mut objects = [player, npc];
+
+    let game = Game {
+        // generate map (not drawn yet)
+        map: make_map(),
+    };
 
     while !tcod.root.window_closed() {
         // clear the screen of the previous frame
@@ -92,23 +177,14 @@ fn main() {
             object.draw(&mut tcod.con);
         } 
 
-        // blit the contents of "con" to the root console and present it
-        blit(
-            &tcod.con,
-            (0, 0),
-            (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut tcod.root,
-            (0, 0),
-            1.0,
-            1.0,
-        );
+        render_all(&mut tcod, &game, &objects);
 
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
 
         // handle keys and exit game if needed
         let player = &mut objects[0];
-        let exit = handle_keys(&mut tcod, player);
+        let exit = handle_keys(&mut tcod, &game, player);
         if exit {
             break;
         } 

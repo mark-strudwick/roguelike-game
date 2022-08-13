43792541a3a269d73825b04785905d99ea8cf866
Mark <markstrudwick@outlook.com>
2022-08-13 19:02:12 +0100

    Rooms

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,14 @@ struct Game {
 }
 
 fn make_map() -> Map {
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    // fill map with "blocked" tiles
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
+    // create two rooms
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
 
     map
 }
@@ -63,6 +67,35 @@ fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
         1.0,
         1.0,
     );
+}
+
+/// A rectangle on the map, used to characterise a room.
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    // make the rectangle tiles passable
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
 }
 
 /// This is a generic object: the player, a monster, an item
@@ -158,7 +191,7 @@ fn main() {
     let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
     let mut tcod = Tcod { root, con };
 
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+    let player = Object::new(25, 23, '@', WHITE);
 
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
 
